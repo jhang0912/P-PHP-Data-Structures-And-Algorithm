8d1e54a8a18dd2fe4554886bbd340c0763f4f529
jhang0912 <fueqaxvk@gmail.com>
2021-08-17 18:03:15 +0800

    1.stack_array.php -edit function push- -edit function pop-

--- data structure/stack(LIFO) array/stack_array.php
+++ data structure/stack(LIFO) array/stack_array.php
@@ -15,9 +15,8 @@ class StackArray
 
   public function push($value)
   {
-    if ($this->indexTop == 0) {
+    if (empty($this->stackArray[0])) {
       $this->stackArray[0] = $value;
-      $this->indexTop++;
     } else {
       $this->indexTop++;
       $this->stackArray[$this->indexTop] = $value;
@@ -39,14 +38,16 @@ class StackArray
   }
 }
 
-$stack = new StackArray([1, 2, 3]);
+$stack = new StackArray([]);
 
 echo '<pre>';
 print_r($stack->stackArray);
-echo $stack->indexTop;
 print_r($stack->push(4));
 print_r($stack->push(5));
 print_r($stack->push(6));
 print_r($stack->pop());
+print_r($stack->pop());
 print_r($stack->push(7));
+print_r($stack->push(8));
+print_r($stack->pop());
 echo '</pre>';
